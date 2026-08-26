6fd0bdbdc2265b3cff4afe2548601898d539a773
OverlordGummyBear <197521801+OverlordGummyBear@users.noreply.github.com>
2026-08-26 14:57:02 +0200

    Add js file for each page and import them in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./styles.css";
-
+import loadMain from "./pages/main.js";
+import loadAbout from "./pages/about.js";
+import loadHome from "./pages/home.js";
---
+++ src/pages/main.js
@@ -0,0 +1,5 @@
+function loadMain(){
+
+}
+
+export default loadMain;
