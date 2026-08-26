86fdad6f54bdec5089ef0fa5f98e9270a858bc39
OverlordGummyBear <197521801+OverlordGummyBear@users.noreply.github.com>
2026-08-26 15:36:37 +0200

    Finish to create main page creation logic

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import "./styles.css";
 import loadMain from "./pages/main.js";
 import loadAbout from "./pages/about.js";
 import loadHome from "./pages/home.js";
+
+const contentDiv = document.querySelector("#content");
+contentDiv.appendChild(loadMain());
